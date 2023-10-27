42624e6bffe8d6fea3fe0eb47a72a32483f9a53f
loftylefty <loftylefty6@gmail.com>
2023-10-27 16:47:36 +0330

    added icon url

--- src/index.js
+++ src/index.js
@@ -29,12 +29,19 @@ function displayWeather(response) {
   let descriptionELEMENT = document.querySelector("#description");
   let humidityELEMENT = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   descriptionELEMENT.innerHTML = response.data.weather[0].description;
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityELEMENT.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let dateElement = document.querySelector("#date");
@@ -42,6 +49,6 @@ let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
 let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-let city = "istanbul";
+let city = "Tehran";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeather);
